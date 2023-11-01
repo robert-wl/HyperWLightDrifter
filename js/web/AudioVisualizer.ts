231c82ca38416151daf822be595ab1aac025e146
yahkerobertkertasnya <robert.wiliam12345@gmail.com>
2023-11-01 19:27:44 +0700

    front additions + game fixed

--- js/web/AudioVisualizer.ts
+++ js/web/AudioVisualizer.ts
@@ -1,46 +1,65 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-class AudioVisualizer {
-    constructor(source) {
+export default class AudioVisualizer {
+    private readonly audio: HTMLAudioElement;
+    private readonly canvas: HTMLCanvasElement;
+    private readonly ctx: CanvasRenderingContext2D;
+    private readonly analyser: AnalyserNode;
+    private readonly bufferLength: number;
+    private readonly dataArray: Uint8Array;
+    private readonly barWidth: number;
+
+    constructor(source: string) {
         this.audio = new Audio(source);
-        this.canvas = $('#audio-visualizer')[0];
-        this.ctx = this.canvas.getContext('2d');
+
+        this.canvas = $('#audio-visualizer')[0] as HTMLCanvasElement;
+        this.ctx = this.canvas.getContext('2d')!;
+
         const audioCtx = new window.AudioContext();
         let audioSource;
+
         this.audio.play().then();
         audioSource = audioCtx.createMediaElementSource(this.audio);
         this.analyser = audioCtx.createAnalyser();
         audioSource.connect(this.analyser);
         this.analyser.connect(audioCtx.destination);
+
         this.analyser.fftSize = Math.pow(2, 9);
         this.bufferLength = this.analyser.frequencyBinCount;
         this.dataArray = new Uint8Array(this.bufferLength);
         this.barWidth = this.canvas.width / (this.bufferLength * 2);
+
         this.animate();
     }
-    animate() {
+
+    private animate() {
         let x = 0;
+
         let barHeight = 0;
         this.ctx.clearRect(0, 0, this.canvas.width, this.canvas.height);
         this.analyser.getByteFrequencyData(this.dataArray);
         for (let i = 0; i < this.bufferLength; i++) {
             barHeight = Math.pow(this.dataArray[i], 3) / 210 ** 2;
+
             const { red, green, blue } = this.getColor(barHeight, i);
+
             // this.ctx.fillStyle = `hsl(${(barHeight / 256) * 360}, 50%, 50%)`;
             this.ctx.fillStyle = `rgb(253, ${green}, ${blue})`;
+
             this.ctx.fillRect(x, this.canvas.height / 2 - barHeight, this.barWidth, barHeight);
             this.ctx.fillStyle = `rgb(103, ${green}, ${blue})`;
             this.ctx.fillRect(x, this.canvas.height / 2, this.barWidth, barHeight);
             x += this.barWidth * 4;
         }
+
         this.ctx.fillStyle = `white`;
+
         requestAnimationFrame(() => this.animate());
     }
-    getColor(barHeight, i) {
+
+    private getColor(barHeight: number, i: number) {
         const red = barHeight + 25 * (i / this.bufferLength);
         const green = 250 * (i / this.bufferLength);
         const blue = 50;
+
         return { red, green, blue };
     }
 }
-exports.default = AudioVisualizer;
--- js/web/web.ts
+++ js/web/web.ts
@@ -1,55 +1,67 @@
-"use strict";
 $(document).ready(() => {
     navbarHandler();
     parallaxHandler();
     carouselHandler();
 });
+
 // new AudioVisualizer({
 // source: '../assets/web/audio/audio8.mp3',
 // });
+
 //test
+
 function carouselHandler() {
     $('.arrow-right').on('click', () => {
         const carousel = $('.inner-container');
         const current = carousel.find('.active');
         const next = current.next();
+
         console.log(carousel.length);
         if (next.length === 0) {
             return;
         }
+
         current.removeClass('active');
         next.addClass('active');
     });
+
     $('.arrow-left').on('click', () => {
         const carousel = $('.inner-container');
         const current = carousel.find('.active');
         const prev = current.prev();
+
         if (prev.prev().length === 0) {
             return;
         }
+
         current.removeClass('active');
         prev.addClass('active');
     });
 }
+
 function navbarHandler() {
     const navbar = $('#navbar');
+
     window.addEventListener('scroll', () => {
         if (window.scrollY > 700) {
             navbar.addClass('navbar-scrolled');
-        }
-        else {
+        } else {
             navbar.removeClass('navbar-scrolled');
         }
     });
 }
+
 function parallaxHandler() {
     $('body').on('scroll', () => {
-        const scrollY = $('#scroll-body').scrollTop();
+        const scrollY = $('#scroll-body').scrollTop()!;
+
         if (scrollY < 3000) {
             return;
         }
+
         const constant = 4200 * (window.innerWidth / 1920);
         const parallaxVal = (scrollY - 4700) * 0.5;
+
         $('.image-cover')[0].style.transform = `translateY(${parallaxVal}px)`;
     });
 }
