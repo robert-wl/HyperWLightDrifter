57f4c4fca7852f25de12a6c90b11b436d77ecdfc
yahkerobertkertasnya <robert.wiliam12345@gmail.com>
2023-11-28 10:33:52 +0700

    2.3.3 feat: final fix

---
+++ js/game/model/utility/enums/SetpieceType.js
@@ -0,0 +1,9 @@
+export var SetpieceType;
+(function (SetpieceType) {
+    SetpieceType["TREE"] = "tree";
+    SetpieceType["ENEMY"] = "enemy";
+    SetpieceType["HEALTH"] = "health";
+    SetpieceType["PLANT"] = "plant";
+    SetpieceType["STONE"] = "stone";
+    SetpieceType["ELEVATOR"] = "elevator";
+})(SetpieceType || (SetpieceType = {}));
